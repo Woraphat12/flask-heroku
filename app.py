0599c8f3051dca2a016a795927aa5dcadb5b6ae9
Woraphat12 <89003899+Woraphat12@users.noreply.github.com>
2021-11-12 13:25:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,35 +3,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-data = [
-        {
-            "id": 1,
-            "library": "Pandas",
-            "language": "Python"
-        },
-        {
-            "id": 2,
-            "library": "requests",
-            "language": "Python"
-        },
-        {
-            "id": 3,
-            "library": "NumPy",
-            "language": "Python"
-        }
-    ]
-
 @app.route('/')
 def hello():
     return "Hello Flask-Herok"
-
-@app.route('/api', methods=['GET'])
-def get_api():
-    return jsonify(data)
-
-@.app.route('/hello/<string:name>')
-def hello():
-    return "สวัสดี"
 
 if __name__ == "__main__":
     app.run(debug=False)
